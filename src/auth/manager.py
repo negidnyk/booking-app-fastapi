2630ecc50b909aad8d3244abeca17317529bcdcc
Negidnyk <artem.yavtushieenko@gmail.com>
2024-06-13 00:40:11 +0300

    [auth] validation fixes 2

--- src/auth/manager.py
+++ src/auth/manager.py
@@ -9,6 +9,53 @@ from src.auth.utils import get_user_db
 from config import RESET_PASS_SECRET
 
 
+
+
+import uuid
+import re
+from typing import Any, Dict, Generic, Optional, Union
+
+import jwt
+from fastapi import Request, Response, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from fastapi_users import exceptions, models, schemas
+from fastapi_users.db import BaseUserDatabase
+from fastapi_users.jwt import SecretType, decode_jwt, generate_jwt
+from fastapi_users.password import PasswordHelper, PasswordHelperProtocol
+from fastapi_users.types import DependencyCallable
+
+RESET_PASSWORD_TOKEN_AUDIENCE = "fastapi-users:reset"
+VERIFY_USER_TOKEN_AUDIENCE = "fastapi-users:verify"
+
+
+
+
+
+
+
+
+
+
+async def validate_password(password: str, user: Union[schemas.UC, models.UP]) -> None:
+    if len(password) < 8:
+        raise HTTPException(status_code=400, detail="Password should contain at least 8 characters")
+
+    if len(password) > 30:
+        raise HTTPException(status_code=400, detail="Password should not contain more than 30 characters")
+
+    if re.search(r'\d', password) and re.search(r'[a-zA-Z]', password):
+        return
+    else:
+        raise HTTPException(status_code=400, detail="Password should contain at least 1 letter and 1 digit")
+    return  # pragma: no cover
+
+
+
+
+
+
+
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     reset_password_token_secret = RESET_PASS_SECRET
     verification_token_secret = RESET_PASS_SECRET
@@ -22,7 +69,13 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         safe: bool = False,
         request: Optional[Request] = None,
     ) -> models.UP:
-        await self.validate_password(user_create.password, user_create)
+        await validate_password(user_create.password, user_create)
+
+
+
+
+
+
 
         existing_user = await self.user_db.get_by_email(user_create.email)
         if existing_user is not None:
