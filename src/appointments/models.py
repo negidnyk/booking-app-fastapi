a3299362eef984956c57c0a3873742a2b43726f2
Negidnyk <artem.yavtushieenko@gmail.com>
2024-09-03 23:47:24 +0300

    updated updateuser schema and added appointments2

---
+++ src/appointments/models.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from sqlalchemy import Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean, MetaData, Double
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+metadata = MetaData()
+
+
+class Appointment(Base):
+    __tablename__ = "appointments"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False, unique=True)
+    master_id = Column(Integer, ForeignKey("user.id", ondelete='CASCADE'))
+    user_id = Column(Integer, ForeignKey("user.id", ondelete='CASCADE'))
+    description = Column(String(200), nullable=True)
+    service_id = Column(Integer, nullable=False)
+    price = Column(Double, nullable=False)
+    booked_at = Column(TIMESTAMP, nullable=False)
+    booked_to = Column(TIMESTAMP, nullable=False)
+    created_at = Column(TIMESTAMP, default=datetime.utcnow, nullable=False)
+    is_approved_by_master = Column(Boolean, default=False, nullable=False)
+    is_visited_by_user = Column(Boolean, default=False, nullable=False)
